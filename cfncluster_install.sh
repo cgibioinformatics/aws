2d6e59c5064927c2c53877f1bb1e4d2f781dfcaf
Jack Yen <37224105+jackyenbioinfo@users.noreply.github.com>
2018-03-26 10:40:13 -0400

    Update cfncluster_install.sh
    update cfncluster_install.sh

--- cfncluster_install.sh
+++ cfncluster_install.sh
@@ -10,18 +10,18 @@ echo Y | apt-get install libfuse-dev libcurl4-openssl-dev libxml++2.6-dev libssl
 
 #git clone https://github.com/s3fs-fuse/s3fs-fuse
 cd /shared/s3fs-fuse/
-./autogen.sh
-./configure 
+sudo ./autogen.sh
+sudo ./configure 
 
-make
-make install
+sudo make
+sudo make install
 
 cd /shared
-
+sudo echo AKIAJOUE6CZYZYQENBJA:D3k6rYpBpVD6v6FOyw5o7NWwnvPmme6VYWcawgJC > /shared/passwd-s3fs
 chmod 600 /shared/passwd-s3fs
 
 s3fs cgix-nc-rna-seq /shared/s3mnt -o passwd_file=/shared/passwd-s3fs -o umask=0000
 
-pip install slackclient
+sudo pip install slackclient
 
 
