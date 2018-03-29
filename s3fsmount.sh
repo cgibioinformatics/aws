874cdbdd7fe247f07b0a263ce44a215ab209f315
Jack Yen <37224105+jackyenbioinfo@users.noreply.github.com>
2018-03-29 15:22:44 -0400

    update s3fs mount file

--- s3fsmount.sh
+++ s3fsmount.sh
@@ -1,1 +1,1 @@
-s3fs cgix-nc-rna-seq /home/ubuntu/s3mnt -o passwd_file=/home/ubuntu/passwd-s3fs -o umask=0000
+sudo s3fs cgix-nc-rna-seq /home/ubuntu/s3mnt -o passwd_file=/home/ubuntu/passwd-s3fs,allow_other,uid=1000,gid=1000,umask=0000 
