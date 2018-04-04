5b975c9892c93521f4437a6138e1a74ccbf85b21
Jack Yen <37224105+jackyenbioinfo@users.noreply.github.com>
2018-04-04 11:10:14 -0400

    update post_isntall script

--- s3fsmount.sh
+++ s3fsmount.sh
@@ -1,1 +1,3 @@
 sudo s3fs cgix-nc-rna-seq /home/ubuntu/s3mnt -o passwd_file=/home/ubuntu/passwd-s3fs,allow_other,uid=1000,gid=1000,umask=0000 
+sudo apt-get install libwww-perl libdatetime-perl
+/home/ubuntu/s3mnt/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --auto-scaling
