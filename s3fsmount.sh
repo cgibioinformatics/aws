5a78995f5f32d4f54aea3bd190a985a7a47c5bea
Jack Yen <37224105+jackyenbioinfo@users.noreply.github.com>
2018-04-04 10:43:25 -0400

    auto-scaling group cloudwatch script

--- s3fsmount.sh
+++ s3fsmount.sh
@@ -1,1 +1,2 @@
 sudo s3fs cgix-nc-rna-seq /home/ubuntu/s3mnt -o passwd_file=/home/ubuntu/passwd-s3fs,allow_other,uid=1000,gid=1000,umask=0000 
+/home/ubuntu/s3mnt/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --auto-scaling
