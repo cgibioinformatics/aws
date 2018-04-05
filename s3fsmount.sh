248fa5be83763784f2a39e8d9412b166e2793878
Jack Yen <37224105+jackyenbioinfo@users.noreply.github.com>
2018-04-05 10:14:23 -0400

    update post_install_script
    update post_install_script adding additional path

--- s3fsmount.sh
+++ s3fsmount.sh
@@ -1,3 +1,5 @@
 sudo s3fs cgix-nc-rna-seq /home/ubuntu/s3mnt -o passwd_file=/home/ubuntu/passwd-s3fs,allow_other,uid=1000,gid=1000,umask=0000 
 echo Y | sudo apt-get install libwww-perl libdatetime-perl
 /home/ubuntu/s3mnt/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --auto-scaling
+export PATH=$PATH:/shared/STAR
+export PATH=$PATH:/shared/cufflink
