269c4ffa5a705039ef90ed193fb397abf35d6f9f
Jack Yen <37224105+jackyenbioinfo@users.noreply.github.com>
2018-10-05 14:59:11 -0400

    Update s3fsmount.sh

--- s3fsmount.sh
+++ s3fsmount.sh
@@ -1,4 +1,5 @@
 sudo s3fs cgix-nc-rna-seq /home/ubuntu/s3mnt -o passwd_file=/home/ubuntu/passwd-s3fs,allow_other,uid=1000,gid=1000,umask=0000 
+sudo apt-get update
 echo Y | sudo apt-get install libwww-perl libdatetime-perl openjdk-8-jre fastqc picard
 /home/ubuntu/s3mnt/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --auto-scaling
 export PATH=$PATH:/shared/STAR
