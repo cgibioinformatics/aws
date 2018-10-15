68be6108ede46df4c358d1afda38c107ade4619f
Jack Yen <37224105+jackyenbioinfo@users.noreply.github.com>
2018-10-15 16:30:14 -0400

    update
    update script

--- s3fsmount_exome.sh
+++ s3fsmount_exome.sh
@@ -2,7 +2,9 @@ sudo s3fs cgix-nc-exome-seq /home/ubuntu/s3mnt -o passwd_file=/home/ubuntu/passw
 echo Y | sudo add-apt-repository ppa:openjdk-r/ppa
 echo Y | sudo apt-get update
 echo Y | sudo apt-get install openjdk-8-jdk
-echo Y | sudo apt-get install libwww-perl libdatetime-perl fastqc picard
+echo Y | sudo apt-get install libwww-perl libdatetime-perl fastqc picard python-pip
+echo Y | sudo pip install numpy
+echo Y | sudo pip install pandas
 /home/ubuntu/s3mnt/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --auto-scaling
 export PATH=$PATH:/shared/STAR
 export PATH=$PATH:/shared/cufflink
