30103ba4a267e55a04ff00066d2a53ed521ff147
Jack Yen <37224105+jackyenbioinfo@users.noreply.github.com>
2018-10-15 14:26:04 -0400

    create new s3fsmount3.sh
    this is the new install

---
+++ s3fsmount3.sh
@@ -0,0 +1,11 @@
+sudo s3fs cgix-nc-rna-seq /home/ubuntu/s3mnt/rna_seq_data -o passwd_file=/home/ubuntu/passwd-s3fs,allow_other,uid=1000,gid=1000,umask=0000 
+sudo s3fs cgix-nc-workspace /home/ubuntu/s3mnt/workspace/py -o passwd_file=/home/ubuntu/passwd-s3fs,allow_other,uid=1000,gid=1000,umask=0000 
+sudo s3fs cgix-nc-exome-seq /home/ubuntu/s3mnt/exome_seq_data -o passwd_file=/home/ubuntu/passwd-s3fs,allow_other,uid=1000,gid=1000,umask=0000 
+echo Y | sudo add-apt-repository ppa:openjdk-r/ppa
+echo Y | sudo apt-get update
+echo Y | sudo apt-get install openjdk-8-jdk
+echo Y | sudo apt-get install libwww-perl libdatetime-perl fastqc picard
+/home/ubuntu/s3mnt/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --auto-scaling
+export PATH=$PATH:/shared/STAR
+export PATH=$PATH:/shared/cufflink
+export PATH=$PATH:/shared/
